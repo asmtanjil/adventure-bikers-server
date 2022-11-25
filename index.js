10fd1fbcb38f49a4fb5521bb49a7b8640936351c
asmtanjil <asm.tanjil@gmail.com>
2022-11-25 12:12:03 +0600

    cors express dotenv mongodb installed in the server

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // Middle Wares
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aes3u62.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/', (req, res) => {
   res.send("Bike Server is Running")
