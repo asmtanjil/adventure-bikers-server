f691eecad4ff7633640a1f46e5fa24119f697e6a
asmtanjil <asm.tanjil@gmail.com>
2022-11-25 20:06:05 +0600

    api created for category of home page

--- index.js
+++ index.js
@@ -11,21 +11,38 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aes3u62.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
 async function run() {
   try {
     const usersCollection = client.db('adventureBiker').collection('users');
-    const categoriesCollection = client.db('adventureBiker').collection('categories');
+    const categoriesCollection = client.db('adventureBiker').collection('category');
     const bikesCollection = client.db('adventureBiker').collection('bikes');
+
+
+    // Load category Items
+    app.get('/categories', async (req, res) => {
+      const query = {}
+      const categories = await categoriesCollection.find(query).toArray()
+      res.send(categories)
+    })
+
+
+
+    // Save User Info by Post Method
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
 
   }
   finally {
 
   }
 }
+run().catch(err => console.error(err))
 
 
 app.get('/', (req, res) => {
