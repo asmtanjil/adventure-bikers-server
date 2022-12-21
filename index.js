f1ae978777295a6e4b62400610ae5acbff6c5564
asmtanjil <asm.tanjil@gmail.com>
2022-12-22 04:44:10 +0600

    JSON Web Token Created

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -15,6 +16,30 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aes3u62.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+function verifyJWT(req, res, next) {
+
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send('Unauthorized Access')
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: 'Forbidden Access' })
+    }
+    req.decoded = decoded;
+    next()
+  })
+
+}
+
+
 
 
 async function run() {
@@ -93,10 +118,48 @@ async function run() {
     })
 
 
+
+
+
+    // JWT Token for users
+    app.get('/jwt', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query)
+
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '7d' })
+        return res.send({ accessToken: token })
+      }
+
+      console.log(user)
+      res.status(403).send({ accessToken: '' })
+    })
+
+
+
+
     // Save User Info by Post Method
     app.post('/users', async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+
+
+    // Save google login users to db
+    app.put('/googleUsers/:email', async (req, res) => {
+      const email = req.params.email;
+      console.log(email)
+      const user = req.body;
+      console.log(user)
+      const filter = { email: email }
+      const options = { upsert: true }
+      const updatedDoc = {
+        $set: user
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc, options)
+      console.log(result)
       res.send(result)
     })
 
@@ -174,12 +237,19 @@ async function run() {
 
 
     // Get Booking Data from Database and send it to client
-    app.get('/bookings', async (req, res) => {
+    app.get('/bookings', verifyJWT, async (req, res) => {
       const email = req.query.email
+      const decodedEmail = req.decoded.email
+
+      if (email !== decodedEmail) {
+        res.status(403).send({ message: 'Forbidden Access' })
+      }
+
       const query = { email: email }
       const result = await ordersCollection.find(query).toArray()
       res.send(result)
     })
+
 
 
   }
